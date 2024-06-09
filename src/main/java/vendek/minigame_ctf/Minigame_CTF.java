2efaf3d7dcfb2ff0745c2b2781143ebaba4c0688
bitweed <j3ywin@gmail.com>
2024-06-09 06:43:54 +0300

    Конфигурационный файл и работа с ним

--- src/main/java/vendek/minigame_ctf/Minigame_CTF.java
+++ src/main/java/vendek/minigame_ctf/Minigame_CTF.java
@@ -2,6 +2,7 @@ package vendek.minigame_ctf;
 
 import org.bukkit.Bukkit;
 import org.bukkit.World;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -9,30 +10,51 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-import vendek.minigame_ctf.commands.PlayerDistributor;
-import vendek.minigame_ctf.commands.StartCTF;
+import vendek.minigame_ctf.pre_game.StartCTF;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public final class Minigame_CTF extends JavaPlugin implements Listener {
 
     private final ScoreUpdater scoreUpdater = new ScoreUpdater("world");
-
     private static Minigame_CTF instance;
+    private static List<GameManager> allGameManagers = new ArrayList<>();
+    
     public static Plugin getInstance() {
         return instance;
+    }
+    public static List<GameManager> getGameManagers() {
+        return allGameManagers;
     }
 
     @Override
     public void onEnable() {
+        saveDefaultConfig();
         instance = this;
 
-        this.getCommand("distributePlayers").setExecutor(new PlayerDistributor());
         this.getCommand("showBossBars").setExecutor(new BossBarManager());
         this.getCommand("start-ctf").setExecutor(new StartCTF());
 
         getServer().getPluginManager().registerEvents(this, this);
+
+        // Получаем все миры из конфига
+        ConfigurationSection worldsConfig = getConfig().getConfigurationSection("worlds");
+        if (worldsConfig != null) {
+            // Создаем объекты миров с режимом, взяв данные из конфига
+            for (String key : worldsConfig.getKeys(false)) {
+                String worldName = worldsConfig.getString(key + ".worldName");
+                GameManager gm = new GameManager(Bukkit.getWorld(worldName));
+                getServer().getPluginManager().registerEvents(gm, this);
+                // Добавим объект режима в список
+                allGameManagers.add(gm);
+            }
+        } else {
+            getLogger().warning("Конфигурация миров не найдена!");
+        }
+        Bukkit.getLogger().info("Список миров режима CTF: " + allGameManagers.toString());
+
 
         // Создание босс баров
         BossBarManager.CreateBossBar();
